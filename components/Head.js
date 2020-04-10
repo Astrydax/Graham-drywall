c6e456b5609c096693ba7c12ec77cfd5f592db1c
Astrydax <astrydax@gmail.com>
2020-04-10 12:11:31 -0400

    undo

--- components/Head.js
+++ components/Head.js
@@ -3,7 +3,7 @@ import NextHead from "next/head";
 const Head = ({ children }) => (
   <NextHead>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    {/*<link rel="icon" type="image/png" href="/images/favicon.png" />*/}
+    <link rel="icon" type="image/png" href="/images/favicon.png" />
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
     <script src="https://unpkg.com/masonry-layout@4/dist/masonry.pkgd.min.js"></script>
     {children}
